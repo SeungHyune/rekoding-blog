d76e58f04573093b5c5ad1d06e9882746eb4cc05
seunghyun <tmdgus7820@naver.com>
2025-02-04 21:47:03 +0900

    💎feat: top, bottom으로 이동 버튼 추가 - layout 페이지에 추가하여 모든 페이지에서 동일하게 보이도록 함

--- src/routes/layouts/Default/Default.tsx
+++ src/routes/layouts/Default/Default.tsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import { Header } from "@/components";
 import useThemeStore from "@/stores/useThemeStore/useThemeStore";
 import "@/styles/theme.css";
+import { ArrowDownIcon, ArrowUpIcon } from "@/components/icons";
+import styles from "./default.module.css";
 
 const DefaultLayout = () => {
   const { theme } = useThemeStore();
@@ -11,6 +13,32 @@ const DefaultLayout = () => {
       <Header />
       <main id="app">
         <Outlet />
+        <div className={styles.scrollButtons}>
+          <button
+            className={styles.up}
+            type="button"
+            onClick={() =>
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              })
+            }
+          >
+            <ArrowUpIcon />
+          </button>
+          <button
+            className={styles.down}
+            type="button"
+            onClick={() =>
+              window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: "smooth",
+              })
+            }
+          >
+            <ArrowDownIcon />
+          </button>
+        </div>
       </main>
     </section>
   );
