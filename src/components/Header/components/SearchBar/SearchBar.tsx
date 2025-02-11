a7169564ad96132fbbf4f80f9c478c2a94e90733
seunghyun <tmdgus7820@naver.com>
2025-02-11 14:42:34 +0900

    💎feat: SearchBar 컴포넌트 검색 기능 추가 - 검색 결과를 react-router-dom의 navigate를 통해 query 데이터로 저장 - 저장된 query를 기반으로 Search 페이지에서 유효한 데이터 filter

--- src/components/Header/components/SearchBar/SearchBar.tsx
+++ src/components/Header/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SearchIcon } from "@components/icons";
 import styles from "./searchBar.module.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useDebounce } from "@/hooks";
 
 const SearchBar = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [searchParams] = useSearchParams();
+
+  const debouncedValue = useDebounce({ value: searchValue, delay: 300 });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (debouncedValue || debouncedValue === "") {
+      navigate(`/search?search=${debouncedValue}`);
+    }
+  }, [debouncedValue]);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(event.target.value);
@@ -11,6 +24,18 @@ const SearchBar = () => {
 
   const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const searchValue = searchParams.get("search") || "";
+
+    if (debouncedValue === searchValue) {
+      return;
+    }
+
+    if (debouncedValue === "" || debouncedValue.trim() === "") {
+      return;
+    }
+
+    navigate(`/search?search=${debouncedValue}`);
   };
 
   return (
