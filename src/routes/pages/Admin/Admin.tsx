83f667265e7e379227dfeb00bdf346007e737984
seunghyun <tmdgus7820@naver.com>
2025-02-16 20:53:32 +0900

    💎feat: Admin 페이지 (아이디, 비밀번호 지우기 & 표시, 비표시 기능 추가) - 로그인 과정에서 아이디를 입력했을 때 모두 지울 수 있는 아이콘 추가 - 비밀번호를 입력했을 때 비밀번호 표시, 비표시 기능 추가

--- src/routes/pages/Admin/Admin.tsx
+++ src/routes/pages/Admin/Admin.tsx
@@ -2,9 +2,13 @@ import { auth } from "@/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import styles from "./admin.module.css";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { CloseCircleIcon, EyeIcon, EyeSlashIcon } from "@/components/icons";
 
 const Admin = () => {
+  const [loginId, setLoginId] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [isError, setIsError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -14,12 +18,14 @@ const Admin = () => {
     event.preventDefault();
     setIsError(false);
 
+    if (loginId.trim().length === 0 || loginPassword.trim().length === 0) {
+      setIsError(true);
+      setErrorMessage("아이디와 비밀번호를 모두 입력해주세요.");
+      return;
+    }
+
     try {
-      await signInWithEmailAndPassword(
-        auth,
-        event.currentTarget.userId.value,
-        event.currentTarget.userPw.value,
-      );
+      await signInWithEmailAndPassword(auth, loginId, loginPassword);
 
       navigate("/");
     } catch (error) {
@@ -27,6 +33,24 @@ const Admin = () => {
       setErrorMessage("아이디 혹은 비밀번호가 올바르지 않습니다.");
       throw new Error(`${error}`);
     }
+  };
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputName = event.target.name;
+
+    if (inputName === "userId") {
+      setLoginId(event.target.value);
+    } else if (inputName === "userPw") {
+      setLoginPassword(event.target.value);
+    }
+  };
+
+  const handleRemoveLoginId = () => {
+    setLoginId("");
+  };
+
+  const handlePasswordVisibleToggle = () => {
+    setIsPasswordVisible((isPasswordVisible) => !isPasswordVisible);
   };
 
   return (
@@ -39,14 +63,52 @@ const Admin = () => {
         <img src="public/images/picture.png" alt="admin login picture" />
       </article>
       <article className={styles.loginContainer}>
+        <Link to="/" title="메인 페이지로 되돌아가기">
+          되돌아가기
+        </Link>
         <form onSubmit={handleLogin}>
-          <input type="text" id="userId" name="userId" placeholder="Id" />
-          <input
-            type="password"
-            id="userPw"
-            name="userPw"
-            placeholder="Password"
-          />
+          <div>
+            <input
+              type="text"
+              id="userId"
+              name="userId"
+              placeholder="Id"
+              value={loginId}
+              onChange={handleChangeInput}
+            />
+            {loginId.length > 0 && (
+              <span onClick={handleRemoveLoginId}>
+                <CloseCircleIcon />
+              </span>
+            )}
+          </div>
+          <div>
+            {isPasswordVisible ? (
+              <input
+                type="text"
+                id="userPw"
+                name="userPw"
+                placeholder="password"
+                value={loginPassword}
+                onChange={handleChangeInput}
+              />
+            ) : (
+              <input
+                type="password"
+                id="userPw"
+                name="userPw"
+                placeholder="Password"
+                value={loginPassword}
+                onChange={handleChangeInput}
+              />
+            )}
+
+            {loginPassword.length > 0 && (
+              <span onClick={handlePasswordVisibleToggle}>
+                {isPasswordVisible ? <EyeSlashIcon /> : <EyeIcon />}
+              </span>
+            )}
+          </div>
           <button type="submit">Login</button>
         </form>
         {isError && <p className={styles.errorMessage}>{errorMessage}</p>}
