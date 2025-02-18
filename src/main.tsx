5d547ad4ce28bf2550a95ffdc40777eee32fc946
seunghyun <tmdgus7820@naver.com>
2025-02-19 00:28:31 +0900

    💎feat: tanstack query provider 설정 - ReactQueryDevtools 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./routes/index.tsx";
 import "./styles/global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 async function enableMocking() {
   if (import.meta.env.VITE_NODE_ENV !== "development") {
@@ -12,10 +14,15 @@ async function enableMocking() {
   return worker.start();
 }
 
+const queryClient = new QueryClient();
+
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Router />
+      </QueryClientProvider>
     </StrictMode>,
   );
 });
