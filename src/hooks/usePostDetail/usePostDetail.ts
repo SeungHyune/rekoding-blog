4d19336f52ed8ee38832c25bb42abde8134ed198
seunghyun <tmdgus7820@naver.com>
2025-02-19 00:50:05 +0900

    🛠️refactor: usePostDetail hook (데이터 패칭 방법 tanstack query로 변경)

--- src/hooks/usePostDetail/usePostDetail.ts
+++ src/hooks/usePostDetail/usePostDetail.ts
@@ -2,41 +2,39 @@ import { FIREBASE_COLLECTION } from "@/constants/firebase/firebase";
 import { db } from "@/firebase";
 import { PostListResponse } from "@/types/response/post";
 import { firebasePostConverter } from "@/utils";
+import { useQuery } from "@tanstack/react-query";
 import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
 
 interface UsePostDetailProps {
   id?: string;
 }
 
+const getPostDetail = async (id: string) => {
+  try {
+    const postDetailDoc = await doc(
+      db,
+      FIREBASE_COLLECTION.POSTS,
+      id,
+    ).withConverter(firebasePostConverter);
+    const response = await getDoc(postDetailDoc);
+    const post = response.data();
+
+    if (!post) {
+      throw new Error("해당 Post는 존재하지 않습니다.");
+    }
+
+    return post;
+  } catch (error) {
+    throw new Error(`error: ${error}`);
+  }
+};
+
 const usePostDetail = ({ id }: UsePostDetailProps) => {
-  const [postDetail, setPostDetail] = useState<PostListResponse | null>(null);
-
-  useEffect(() => {
-    const getPostDetail = async () => {
-      try {
-        if (!id) return;
-
-        const postDetailDoc = await doc(
-          db,
-          FIREBASE_COLLECTION.POSTS,
-          id,
-        ).withConverter(firebasePostConverter);
-        const response = await getDoc(postDetailDoc);
-        const post = response.data();
-
-        if (!post) {
-          throw new Error("해당 Post는 존재하지 않습니다.");
-        }
-
-        setPostDetail(post);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    getPostDetail();
-  }, [id]);
+  const { data: postDetail } = useQuery<PostListResponse>({
+    queryKey: ["postDetail", id],
+    queryFn: () => getPostDetail(id as string),
+    enabled: !!id,
+  });
 
   return {
     postDetail,
