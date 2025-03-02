57fdef59e3a3340f8f61880ad85ea009f6e1f6ad
seunghyun <tmdgus7820@naver.com>
2025-03-02 18:29:48 +0900

    💎feat: ReactEditor 컴포넌트 제작 - 블로그 포스트 내용을 작성하는 editor 컴포넌트입니다. - contentEditable 속성을 통해 콘텐츠 내용을 작성할 수 있도록 만들었습니다. (contenteditable 내부에서 엔터를 치면 div 태그가 자체적으로 생성되며 줄바꿈 되는데, div 태그를 무시할 수 있도록 내부적으로 br 태그를 추가했습니다.) - ReactEditor 컴포넌트를 사용하는 쪽에서 content 내용 상태를 만들어서 내부 onInput 이벤트를 통해 내부 innerText를 상태에 저장합니다. (handleInputContent 핸들러 prop으로 전달 필수) - 내부적으로 입력된 내용을 외부에서 블로그 글 작성 시 어떻게 보이는지 preview로 보이도록 작업

---
+++ src/components/ReactEditor/ReactEditor.tsx
@@ -0,0 +1,49 @@
+import { CSSProperties } from "react";
+
+interface ReactEditorProps {
+  style?: CSSProperties;
+  handleInputContent: (event: React.ChangeEvent<HTMLDivElement>) => void;
+}
+
+const ReactEditor = ({ style, handleInputContent }: ReactEditorProps) => {
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+
+      const selection = window.getSelection();
+
+      if (!selection || selection?.rangeCount === 0) return;
+
+      const range = selection.getRangeAt(0);
+      range.deleteContents();
+
+      const br = document.createElement("br");
+      range.insertNode(br);
+
+      const newNode = document.createTextNode("\n");
+      range.insertNode(newNode);
+
+      range.setStartAfter(newNode);
+      range.collapse(true);
+
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        outline: "none",
+        whiteSpace: "pre-wrap",
+        ...style,
+      }}
+      contentEditable
+      onInput={handleInputContent}
+      onKeyDown={handleKeyDown}
+    />
+  );
+};
+
+export default ReactEditor;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./Header/Header";
 export { default as Modal } from "./Modal/Modal";
 export { default as Message } from "./Message/Message";
 export { default as AlertModal } from "./AlertModal/AlertModal";
+export { default as ReactEditor } from "./ReactEditor/ReactEditor";
