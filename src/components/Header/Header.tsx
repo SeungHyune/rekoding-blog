f753ef7ca1c9ac7820a1186a83b18e8879e1adaf
seunghyun <tmdgus7820@naver.com>
2025-03-03 23:26:24 +0900

    🛠️refactor: Header 컴포넌트 반응형 퍼블리싱으로 인한 마크업 구조 변경 - Header 1차 반응형 퍼블리싱 완료 (모바일에서 보여줄 레이아웃 설계) - 현재 940px 이하에서 SearchBar 컴포넌트 출력을 어떻게 보여줄 지 고민하고 있어 해당 컴포넌트는 940px 이하에서 임시로 display: none 처리해놨습니다.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,52 +4,97 @@ import {
   EmailIcon,
   EmailDarkIcon,
   GithubDarkIcon,
+  NavMenuIcon,
+  NavMenuCloseIcon,
 } from "@/components/icons";
 import { SwitchButton, SearchBar } from "./components";
 import styles from "./header.module.css";
 import useThemeStore from "@/stores/useThemeStore/useThemeStore";
+import { useEffect } from "react";
+import { useToggle } from "@/hooks";
 
 const Header = () => {
   const { theme } = useThemeStore();
+
+  const {
+    isToggle: isNavToggle,
+    handleToggleClose: handleNavToggleOff,
+    handleToggle: handleNavToggle,
+  } = useToggle({ isDefaultToggleValue: true });
+
+  useEffect(() => {
+    if (window.innerWidth <= 940) {
+      console.log("effect");
+      handleNavToggleOff();
+    }
+  }, []);
+
+  const handleNavShowToggle = () => {
+    handleNavToggle();
+  };
+
+  console.log("isNavToggle", isNavToggle);
+
   return (
     <header
       className={`${styles.header} ${theme === "DARK" ? "dark" : "light"}`}
     >
       <section className={styles.headerContainer}>
-        <article className={styles.headerNavigation}>
-          <Link className={styles.logo} to="/" title="Logo">
-            <div className={styles.imgBox}>
-              <img src="/logo.png" alt="블로그 로고 이미지" />
+        <Link className={styles.logo} to="/" title="Logo">
+          <div className={styles.imgBox}>
+            <img src="/logo.png" alt="블로그 로고 이미지" />
+          </div>
+          <div className={styles.textBox}>
+            <span>REKODING</span>
+            <span>DEV</span>
+          </div>
+        </Link>
+
+        <article className={styles.headerInfoContainer}>
+          {isNavToggle && (
+            <article className={`${styles.headerNavigation} headerNavigation`}>
+              <nav className={`${styles.navigation} navigation`}>
+                <ul>
+                  <li>
+                    <NavLink to="/">Home</NavLink>
+                  </li>
+                </ul>
+              </nav>
+
+              <ul className={`${styles.infoList} infoList`}>
+                <li>
+                  <Link to="https://github.com/SeungHyune" target="_blank">
+                    {theme === "DARK" ? <GithubDarkIcon /> : <GithubIcon />}
+                    <span>Github</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="mailto:tmdgus7820@naver.com">
+                    {theme === "DARK" ? <EmailDarkIcon /> : <EmailIcon />}
+                    <span>Email</span>
+                  </Link>
+                </li>
+              </ul>
+            </article>
+          )}
+          <article className={styles.headerInfo}>
+            <div className={styles.switchButton}>
+              <SwitchButton />
             </div>
-            <div className={styles.textBox}>
-              <span>REKODING</span>
-              <span>DEV</span>
+            <div className={styles.searchBar}>
+              <SearchBar />
             </div>
-          </Link>
-          <nav className={styles.navigation}>
-            <ul>
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-            </ul>
-          </nav>
+          </article>
         </article>
-        <article className={styles.headerInfo}>
-          <ul className={styles.infoList}>
-            <li>
-              <Link to="https://github.com/SeungHyune" target="_blank">
-                {theme === "DARK" ? <GithubDarkIcon /> : <GithubIcon />}
-              </Link>
-            </li>
-            <li>
-              <Link to="mailto:tmdgus7820@naver.com">
-                {theme === "DARK" ? <EmailDarkIcon /> : <EmailIcon />}
-              </Link>
-            </li>
-          </ul>
-          <SwitchButton />
-          <SearchBar />
-        </article>
+
+        <button
+          className={`${styles.navMenuBtn} navMenuBtn`}
+          type="button"
+          onClick={handleNavShowToggle}
+        >
+          {isNavToggle ? "닫기" : "메뉴"}
+          {isNavToggle ? <NavMenuCloseIcon /> : <NavMenuIcon />}
+        </button>
       </section>
     </header>
   );
