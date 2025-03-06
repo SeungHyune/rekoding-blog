ad76ad2c921c68b19eaf502be639c69e124fb951
seunghyun <tmdgus7820@naver.com>
2025-03-06 23:05:03 +0900

    💎feat: ReactMarkdownPreview 컴포넌트에 플러그인 추가(remark-breaks) - 줄바꿈 \n을 <br> 태그로 변환

--- src/components/ReactMarkdownPreview/ReactMarkdownPreview.tsx
+++ src/components/ReactMarkdownPreview/ReactMarkdownPreview.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from "react-markdown";
 import rehypeSlug from "rehype-slug";
+import remarkBreaks from "remark-breaks";
 import styles from "./reactMarkdownPreview.module.css";
 
 interface ReactMarkdownPreviewProps {
@@ -9,7 +10,12 @@ interface ReactMarkdownPreviewProps {
 const ReactMarkdownPreview = ({ content }: ReactMarkdownPreviewProps) => {
   return (
     <div className={`${styles.reactMarkdown} reactMarkdown`}>
-      <ReactMarkdown rehypePlugins={[rehypeSlug]}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        rehypePlugins={[rehypeSlug]}
+        remarkPlugins={[remarkBreaks]}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 };
